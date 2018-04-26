7373ecacc3d1f7de9b97300a801898e8aa3c1a4a
XuYibo <xuyibo@anji-plus.com>
2018-04-26 13:48:33 +0800

    新增用户校验

--- src/main/java/com/yibo/security/service/impl/UserServiceImpl.java
+++ src/main/java/com/yibo/security/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.yibo.security.entity.UserEntity;
 import com.yibo.security.service.UserService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import javax.annotation.Resource;
 
@@ -23,6 +24,8 @@ public class UserServiceImpl implements UserService {
     @Transactional
     public void insertUser(UserEntity userEntity) {
         userEntity.setId(null);
+        Assert.hasText(userEntity.getUsername(), "用户名不能为空！");
+        Assert.hasText(userEntity.getPassword(), "密码不能为空！");
         userDao.insertSelective(userEntity);
     }
 }
